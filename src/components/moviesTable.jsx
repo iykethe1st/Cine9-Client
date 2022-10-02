6628215d530c3037cb0e0db81cdfcba7d8fb24d1
Ikenna Udemezue <iykethe1st@gmail.com>
2022-10-02 18:27:21 +0100

    Table Extraction

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import LikeBtn from "./common/likeBtn";
 import TableBody from "./common/tableBody";
 import TableHeader from "./common/tableHeader";
+import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
@@ -34,15 +35,12 @@ class MoviesTable extends Component {
   render() {
     const { movies, sortColumn, onSort } = this.props;
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-
-        <TableBody data={movies} columns={this.columns} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      ></Table>
     );
   }
 }
