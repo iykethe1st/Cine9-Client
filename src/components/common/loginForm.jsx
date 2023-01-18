059e2028a0fe69177da28dab23302bb56435954c
Ikenna Udemezue <iykethe1st@gmail.com>
2023-01-18 09:54:58 +0100

    login error handling

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -17,8 +17,16 @@ class LoginForm extends Form {
 
   doSubmit = async () => {
     // console.log("Submitted to server");
-    const { data } = this.state;
-    await login(data.username, data.password);
+    try {
+      const { data } = this.state;
+      await login(data.username, data.password);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
